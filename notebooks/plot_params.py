5123551b65ba50a34258bfa9fc6f64eea52a27ca
Jonathan Williams <jpw.ism@gmail.com>
2022-01-21 15:24:41 -1000

    added example data

---
+++ notebooks/plot_params.py
@@ -0,0 +1,13 @@
+# customize plots the way I like them :)
+import matplotlib as mpl
+mpl.rcParams['xtick.labelsize'] = 14
+mpl.rcParams['ytick.labelsize'] = 14
+mpl.rcParams['axes.labelsize'] = 18
+mpl.rcParams['legend.fontsize'] = 18
+mpl.rcParams['xtick.major.size'] = 6
+mpl.rcParams['xtick.minor.size'] = 4
+mpl.rcParams['xtick.major.width'] = 2
+mpl.rcParams['ytick.major.size'] = 6
+mpl.rcParams['ytick.minor.size'] = 4
+mpl.rcParams['ytick.major.width'] = 2
+mpl.rcParams['axes.linewidth'] = 2
